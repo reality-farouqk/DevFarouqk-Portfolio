80bdfbc6aa17092b351f212276dc5f05d71587a3
Umar Sulaiman Mailafiya <mailafiyaumarsulaiman@gmail.com>
2022-07-08 15:25:10 +0100

    update from mobile

--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
-// Navigation setting 
-const primaryNav = document.querySelector(".primary-nav");
-const navToggle = document.querySelector(".mobile-nav-toggle");
+//selectElement
+function selectElement(e) {
+  return document.querySelector(e);
+}
+
+// Navigation setting
+const primaryNav = selectElement(".primary-nav");
+const navToggle = selectElement(".mobile-nav-toggle");
 
 navToggle.addEventListener("click", () => {
-    const visibility = primaryNav.getAttribute("data-visible");
+  const visibility = primaryNav.getAttribute("data-visible");
 
-    if (visibility === "false") {
-        primaryNav.setAttribute('data-visible', true)
-        navToggle.setAttribute('aria-expanded', true)
-    } else {
-        primaryNav.setAttribute('data-visible', false)
-        navToggle.setAttribute('aria-expanded', false)
-    }
-})
+  if (visibility === "false") {
+    primaryNav.setAttribute('data-visible', true)
+    navToggle.setAttribute('aria-expanded', true)
+  } else {
+    primaryNav.setAttribute('data-visible', false)
+    navToggle.setAttribute('aria-expanded', false)
+  }
+});
 
+const navLink = selectElement(".nav ul li a");
+function activePageLink() {
+  for (let i = 0; i < navLink.length; i++) {
+    if (window.location.href == navLink[i].href) navLink[i].classList.add("active");
+  }
+}
+activePageLink();
